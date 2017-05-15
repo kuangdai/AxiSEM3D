5c31b88abb7d53c6a466408700c0119e48e3fb93
kuangdai <kuangdal@earth.ox.ac.uk>
2017-05-15 22:41:35 +0100

    refine XTimer

--- SOLVER/src/preloop/utilities/XTimer.cpp
+++ SOLVER/src/preloop/utilities/XTimer.cpp
@@ -6,64 +6,101 @@
 #include "XMPI.h"
 
 std::string XTimer::mFileName;
-std::vector<MyBoostTimer> XTimer::mTimers;
 std::fstream XTimer::mFile;
+std::vector<MyBoostTimer> XTimer::mTimers;
 bool XTimer::mEnabled = false;
 
-void XTimer::initialize(std::string fileName, int nLevels) {
-    if (!XMPI::root()) return;
-    // mFile = std::fstream(fileName, std::fstream::out);
-    mFileName = fileName;
-    mTimers.clear();
-    for (int i = 0; i < nLevels; i++) 
-        mTimers.push_back(MyBoostTimer());
+void XTimer::initialize(const std::string &fileName, size_t nLevels) {
     mEnabled = false;
+    if (XMPI::root()) {
+        mFileName = fileName;
+        mTimers.clear();
+        for (size_t i = 0; i < nLevels; i++) {
+            mTimers.push_back(MyBoostTimer());
+        }
+    }
 }
 
 void XTimer::openFile() {
-    if (!XMPI::root()) return;
-    if (!mFile.is_open()) mFile.open(mFileName, std::fstream::out);
+    if (XMPI::root()) {
+        if (!mFile.is_open()) {
+            mFile.open(mFileName, std::fstream::out);
+        }
+    }
 }
 
 void XTimer::finalize() {
-    if (!XMPI::root()) return;
-    if (!mFile.is_open()) mFile.close();
+    if (XMPI::root()) {
+        if (mFile.is_open()) {
+            mFile.close();
+        }
+    }
 }
 
-void XTimer::begin(std::string name, int level, bool barrier) {
-    if (!mEnabled) return;
-    if (barrier) XMPI::barrier();
-    if (!XMPI::root()) return;
-    for (int i = 0; i < level; i++) mFile << "    ";
-    mFile << name << " begins..." << std::endl;
-    mTimers[level].start();
+void XTimer::begin(const std::string &name, size_t level, bool barrier) {
+    if (!mEnabled) {
+        return;
+    }
+    if (barrier) {
+        XMPI::barrier();
+    }
+    if (XMPI::root()) {
+        for (size_t i = 0; i < level; i++) {
+            mFile << "    ";
+        }
+        mFile << name << " begins..." << std::endl;
+        mTimers[level].start();
+    }
 }
 
-void XTimer::end(std::string name, int level, bool barrier) {
-    if (!mEnabled) return;
-    if (barrier) XMPI::barrier();
-    if (!XMPI::root()) return;
-    mTimers[level].stop();
-    for (int i = 0; i < level; i++) mFile << "    ";
-    mFile << name << " finishes. Elapsed seconds = " << mTimers[level].elapsed() << std::endl;
+void XTimer::end(const std::string &name, size_t level, bool barrier) {
+    if (!mEnabled) {
+        return;
+    }
+    if (barrier) {
+        XMPI::barrier();
+    }
+    if (XMPI::root()) {
+        mTimers[level].stop();
+        for (size_t i = 0; i < level; i++) {
+            mFile << "    ";
+        }
+        mFile << name << " finishes. Elapsed seconds = " 
+            << mTimers[level].elapsed() << std::endl;
+    }
 }
 
-void XTimer::pause(int level) {
-    if (!XMPI::root() || !mEnabled) return;
-    mTimers[level].stop();
+void XTimer::pause(size_t level) {
+    if (!mEnabled) {
+        return;
+    }
+    if (XMPI::root()) {
+        mTimers[level].stop();
+    }
 }
 
-void XTimer::resume(int level) {
-    if (!XMPI::root() || !mEnabled) return;
-    mTimers[level].resume();
+void XTimer::resume(size_t level) {
+    if (!mEnabled) {
+        return;
+    }
+    if (XMPI::root()) {
+        mTimers[level].resume();
+    }
 }
 
+///////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
 void MyBoostTimer::stop() {
-    if (running()) mTimePoints.push_back(std::chrono::high_resolution_clock::now());
+    if (running()) {
+        mTimePoints.push_back(std::chrono::high_resolution_clock::now());
+    }
 }
 
 void MyBoostTimer::resume() {
-    if (!running()) mTimePoints.push_back(std::chrono::high_resolution_clock::now());
+    if (!running()) {
+        mTimePoints.push_back(std::chrono::high_resolution_clock::now());
+    }
 }
 
 void MyBoostTimer::clear() {
@@ -72,8 +109,8 @@ void MyBoostTimer::clear() {
 
 double MyBoostTimer::elapsed() {
     double elap = 0.;
-    int pairs = mTimePoints.size() / 2;
-    for (int i = 0; i < pairs; i++) {
+    size_t pairs = mTimePoints.size() / 2;
+    for (size_t i = 0; i < pairs; i++) {
         elap += (std::chrono::duration_cast<std::chrono::duration<double>>
             (mTimePoints[2 * i + 1] - mTimePoints[2 * i])).count();
     }
@@ -90,8 +127,9 @@ double MyBoostTimer::getClockResolution() {
     std::chrono::high_resolution_clock::time_point start_time, current_time;
     start_time = std::chrono::high_resolution_clock::now();
     current_time = start_time;
-    while (current_time == start_time)
+    while (current_time == start_time) {
         current_time = std::chrono::high_resolution_clock::now();
+    }
     return (std::chrono::duration_cast<std::chrono::duration<double>>
         (current_time - start_time)).count();
 }
--- SOLVER/src/preloop/utilities/XTimer.h
+++ SOLVER/src/preloop/utilities/XTimer.h
@@ -26,20 +26,21 @@ private:
 
 class XTimer {
 public:
-    static void initialize(std::string fileName, int nLevels);
+    static void initialize(const std::string &fileName, size_t nLevels);
     static void finalize();
-    static void begin(std::string procName, int level, bool barrier = false);
-    static void end(std::string procName, int level, bool barrier = true);
+    static void begin(const std::string &procName, size_t level, bool barrier = false);
+    static void end(const std::string &procName, size_t level, bool barrier = true);
+    static void pause(size_t level);
+    static void resume(size_t level);
+    
     static void enable() {openFile(); mEnabled = true;};
     static void disable() {mEnabled = false;};
-    static void pause(int level);
-    static void resume(int level);
     
 private:
     static void openFile();
     static std::string mFileName;
-    static std::vector<MyBoostTimer> mTimers;
     static std::fstream mFile; 
+    static std::vector<MyBoostTimer> mTimers;
     static bool mEnabled;
 };
 
