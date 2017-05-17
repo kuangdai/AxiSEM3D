9608fdc6670c6a2423ce6ce8506b42e0694010ee
kuangdai <kuangdal@earth.ox.ac.uk>
2017-05-17 20:06:27 +0100

    oceanload

--- SOLVER/src/3d_model/3d_oceanload/crust1/OceanLoad3D_crust1.cpp
+++ SOLVER/src/3d_model/3d_oceanload/crust1/OceanLoad3D_crust1.cpp
@@ -4,19 +4,20 @@
 // http://igppweb.ucsd.edu/~gabi/crust1.html
 
 #include "OceanLoad3D_crust1.h"
-#include "Volumetric3D_crust1.h"
 #include <sstream>
 #include <fstream>
 #include "XMPI.h"
 #include "XMath.h"
+#include "Geodesy.h"
+#include "Parameters.h"
 
-const size_t OceanLoad3D_crust1::sNLayer = 9;
-const size_t OceanLoad3D_crust1::sNLat = 180;
-const size_t OceanLoad3D_crust1::sNLon = 360;
+const int OceanLoad3D_crust1::sNLayer = 9;
+const int OceanLoad3D_crust1::sNLat = 180;
+const int OceanLoad3D_crust1::sNLon = 360;
 
 void OceanLoad3D_crust1::initialize() {
     // read raw data
-    size_t nrow = sNLat * sNLon;
+    int nrow = sNLat * sNLon;
     RDMatXX elevation = RDMatXX::Zero(nrow, sNLayer);
     if (XMPI::root()) {
         std::string fname = projectDirectory + "/src/3d_model/3d_volumetric/crust1/data/crust1.bnds";
@@ -25,8 +26,8 @@ void OceanLoad3D_crust1::initialize() {
             throw std::runtime_error("OceanLoad3D_crust1::initialize || "
                 "Error opening crust1.0 data file: ||" + fname);
         }
-        for (size_t i = 0; i < nrow; i++) {
-            for (size_t j = 0; j < sNLayer; j++) {
+        for (int i = 0; i < nrow; i++) {
+            for (int j = 0; j < sNLayer; j++) {
                 fs >> elevation(i, j);
             }
         }
@@ -36,7 +37,7 @@ void OceanLoad3D_crust1::initialize() {
     XMPI::bcastEigen(elevation);
     
     // water depth
-    size_t colWaterBot = 1;
+    int colWaterBot = 1;
     if (mIncludeIceAsWater) {
         colWaterBot = 2;
     }
@@ -46,13 +47,12 @@ void OceanLoad3D_crust1::initialize() {
     if (mBenchmarkSPECFEM) {
         depthVec = elevation.col(2) * 1e3;
         mGaussianOrder = 0;
-        mNPointInterp = 2;
         mIncludeIceAsWater = false;
         mGeographic = false;
     }
     
     RDMatXX depth(sNLat, sNLon);
-    for (size_t i = 0; i < sNLat; i++) {
+    for (int i = 0; i < sNLat; i++) {
         depth.row(i) = depthVec.block(i * sNLon, 0, sNLon, 1).transpose();
     } 
         
@@ -76,8 +76,18 @@ void OceanLoad3D_crust1::initialize() {
     mDepth.row(0).fill(depth.row(0).sum() / sNLon);
     mDepth.row(sNLat).fill(depth.row(sNLat - 1).sum() / sNLon);
     // interp at integer theta
-    for (size_t i = 1; i < sNLat; i++) {
+    for (int i = 1; i < sNLat; i++) {
         mDepth.row(i) = (depth.row(i - 1) + depth.row(i)) * .5;
+    }
+    
+    // grid lat and lon
+    mGridLat = RDColX(sNLat + 1);
+    mGridLon = RDColX(sNLon + 1); // one bigger than data
+    for (int i = 0; i < sNLat + 1; i++) {
+        mGridLat[i] = i * 1. - 90.;
+    }
+    for (int i = 0; i < sNLon + 1; i++) {
+        mGridLon[i] = i * 1. - 180.;
     }
     
     //////////// plot raw data ////////////  
@@ -91,14 +101,13 @@ void OceanLoad3D_crust1::initialize() {
 
 void OceanLoad3D_crust1::initialize(const std::vector<std::string> &params) {
     try {
-        size_t ipar = 0;
+        int ipar = 0;
         const std::string source = "OceanLoad3D_crust1::initialize";
-        XMath::castValue(mGaussianOrder, params.at(ipar++), source);
-        XMath::castValue(mGaussianDev, params.at(ipar++), source);
-        XMath::castValue(mNPointInterp, params.at(ipar++), source);
-        XMath::castValue(mGeographic, params.at(ipar++), source);
-        XMath::castValue(mIncludeIceAsWater, params.at(ipar++), source);
-        XMath::castValue(mBenchmarkSPECFEM, params.at(ipar++), source);
+        Parameters::castValue(mIncludeIceAsWater, params.at(ipar++), source);
+        Parameters::castValue(mGaussianOrder, params.at(ipar++), source);
+        Parameters::castValue(mGaussianDev, params.at(ipar++), source);
+        Parameters::castValue(mGeographic, params.at(ipar++), source);
+        Parameters::castValue(mBenchmarkSPECFEM, params.at(ipar++), source);
     } catch (std::out_of_range) {
         // nothing
     }
@@ -108,20 +117,37 @@ void OceanLoad3D_crust1::initialize(const std::vector<std::string> &params) {
 double OceanLoad3D_crust1::getOceanDepth(double theta, double phi) const {
     // convert theta to co-latitude  
     if (mGeographic) {
-        theta = pi / 2. - XMath::theta2Lat(theta, 0.) * degree;
+        theta = pi / 2. - Geodesy::theta2Lat_d(theta, 0.) * degree;
     }
     
-    // interpolation on sphere
-    double depth = 0.;
-    std::vector<size_t> ilat, ilon;
-    std::vector<double> wlat, wlon;
-    Volumetric3D_crust1::interpThetaPhi(theta, phi, mNPointInterp, ilat, ilon, wlat, wlon);
-    for (size_t i = 0; i < mNPointInterp; i++) {
-        for (size_t j = 0; j < mNPointInterp; j++) {
-            double weight = wlat[i] * wlon[j];
-            depth += weight * mDepth(ilat[i], ilon[j]);
-        }
+    // regularise
+    double lat = 90. - theta / degree;
+    double lon = phi / degree;
+    if (lon > 180.) {
+        lon -= 360.;
     }
+    XMath::checkLimits(lat, -90., 90.);
+    XMath::checkLimits(lon, -180., 180.);
+    
+    // interpolation on sphere
+    int llat0, llon0, llat1, llon1;
+    double wlat0, wlon0, wlat1, wlon1;
+    XMath::interpLinear(lat, mGridLat, llat0, wlat0);
+    XMath::interpLinear(lon, mGridLon, llon0, wlon0);    
+    llat1 = llat0 + 1;
+    llon1 = llon0 + 1;
+    wlat1 = 1. - wlat0;
+    wlon1 = 1. - wlon1;
+    if (llon1 == sNLon) {
+        llon1 = 0;
+    }
+    
+    double depth = 0.;
+    depth += mDepth(llat0, llon0) * wlat0 * wlon0;
+    depth += mDepth(llat1, llon0) * wlat1 * wlon0;
+    depth += mDepth(llat0, llon1) * wlat0 * wlon1;
+    depth += mDepth(llat1, llon1) * wlat1 * wlon1;
+    
     if (mBenchmarkSPECFEM) {
         double elevation = depth;
         double MINIMUM_THICKNESS_3D_OCEANS = 50.;
@@ -131,6 +157,7 @@ double OceanLoad3D_crust1::getOceanDepth(double theta, double phi) const {
             depth = std::abs(elevation);
         }
     }
+    
     return depth;
 }
 
@@ -138,11 +165,10 @@ std::string OceanLoad3D_crust1::verbose() const {
     std::stringstream ss;
     ss << "\n======================= 3D OceanLoad =======================" << std::endl;
     ss << "  Model Name            =   Crust 1.0" << std::endl;
+    ss << "  Add Ice as Water      =   " << (mIncludeIceAsWater ? "YES" : "NO") << std::endl;
     ss << "  Smoothing Order       =   " << mGaussianOrder << std::endl;
     ss << "  Smoothing Intensity   =   " << mGaussianDev << std::endl;
-    ss << "  Num. Interp. Points   =   " << mNPointInterp << std::endl;
     ss << "  Use Geographic        =   " << (mGeographic ? "YES" : "NO") << std::endl;
-    ss << "  Add Ice as Water      =   " << (mIncludeIceAsWater ? "YES" : "NO") << std::endl;
     if (mBenchmarkSPECFEM) {
         ss << "  Using SPECFEM method, computing ocean depth by elevation." << std::endl;
     }
