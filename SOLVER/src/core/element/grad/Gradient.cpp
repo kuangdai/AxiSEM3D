c34f3bd1bcb5f38a654568cc47cff7926c1cd402
kuangdai <kuangdal@earth.ox.ac.uk>
2017-05-26 22:41:03 +0100

    bug

--- SOLVER/src/core/element/grad/Gradient.cpp
+++ SOLVER/src/core/element/grad/Gradient.cpp
@@ -262,7 +262,6 @@ void Gradient::computeGrad6(const vec_ar3_CMatPP &ui, vec_ar6_CMatPP &eij, int N
     }   
 }
 
-#include <iostream>
 void Gradient::computeQuad6(vec_ar3_CMatPP &fi, const vec_ar6_CMatPP &sij, int Nu, int nyquist) const {
     // hardcode for mbeta = 0
     static RMatPP X0R, X1R, X2R, Y0R, Y1R, Y2R; 
@@ -279,10 +278,6 @@ void Gradient::computeQuad6(vec_ar3_CMatPP &fi, const vec_ar6_CMatPP &sij, int N
         fi[0][0].real() += (*sG_xii).col(0) * mDzDeta.row(0).schur(sij[0][1].real().row(0));
         fi[0][1].real() -= (*sG_xii).col(0) * mDzDeta.row(0).schur(sij[0][5].real().row(0));
     }
-    
-    std::cout << sij[0][0]<<std::endl;
-    std::cout << fi[0][0]<<std::endl;
-    exit(0);
     
     // mbeta > 0
     static CMatPP g0, g1, g2, X0, X1, X2, Y0, Y1, Y2;
